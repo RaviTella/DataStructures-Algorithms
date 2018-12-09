404eec0e27d2bb0a21d7337c8362ca691c8bc232
RaviTella <ravi.tella@outlook.com>
2018-12-09 13:41:05 -0600

    formatted

--- src/dataStructures/generalPurpose/trees/binary/search/BinarySearchTreeByRef.java
+++ src/dataStructures/generalPurpose/trees/binary/search/BinarySearchTreeByRef.java
@@ -5,118 +5,112 @@ package dataStructures.generalPurpose.trees.binary.search;
  * Binary Search Tree using references 
  * 
  */
-public class BinarySearchTreeByRef  {
-  Node root;
+public class BinarySearchTreeByRef {
+	Node root;
 
-  public BinarySearchTreeByRef() {
-    this.root = null;
-  }
+	public BinarySearchTreeByRef() {
+		this.root = null;
+	}
 
+	public void insert(int key, String value) {
+		Node nNode = new Node(key, value, null, null);
+		if (root == null) {
+			root = nNode;
+		} else {
+			Node cNode = root;
+			while (true) {
+				Node pNode = cNode; // saving the current node reference to
+									// pNode
+				if (nNode.key < cNode.key) {
+					cNode = cNode.leftChild;
+					if (cNode == null) { // reached vacant spot
+						pNode.leftChild = nNode; // setting the new node as
+													// pNode left Child
+						return;
+					}
+				} else {
+					cNode = cNode.rightChild;
+					if (cNode == null) { // reached vacant spot
+						pNode.rightChild = nNode; // setting the new node as
+													// pNode right Child
+						return;
+					}
+				}
+			}
+		}
+	}
 
-  public void insert(int key, String value) {
-    Node nNode = new Node(key, value, null, null);
-    if (root == null) {
-      root = nNode;
-    } else {
-      Node cNode = root;
-      while (true) {
-        Node pNode = cNode; // saving the current node reference to
-                            // pNode
-        if (nNode.key < cNode.key) {
-          cNode = cNode.leftChild;
-          if (cNode == null) { // reached vacant spot
-            pNode.leftChild = nNode; // setting the new node as
-                                     // pNode left Child
-            return;
-          }
-        } else {
-          cNode = cNode.rightChild;
-          if (cNode == null) { // reached vacant spot
-            pNode.rightChild = nNode; // setting the new node as
-                                      // pNode right Child
-            return;
-          }
-        }
-      }
-    }
-  }
+	public Node find(int key) {
+		Node cNode;
+		if (root == null) {
+			return null;
+		} else {
+			cNode = root;
+			while (cNode != null && cNode.key != key) {
+				if (key < cNode.key) { // go left
+					cNode = cNode.leftChild;
+				} else { // go right
+					cNode = cNode.rightChild;
+				}
+			}
+		}
+		return cNode;
+	}
 
+	public Node minimum() {
+		Node cNode = null;
+		if (root == null) {
+			return null;
+		} else {
+			cNode = root;
+			while (cNode.leftChild != null) { // keep going left till the left
+												// child of a node is NULL
+				cNode = cNode.leftChild;
+			}
+		}
+		return cNode;
+	}
 
-  public Node find(int key) {
-    Node cNode;
-    if (root == null) {
-      return null;
-    } else {
-      cNode = root;
-      while (cNode != null && cNode.key != key) {
-        if (key < cNode.key) { // go left
-          cNode = cNode.leftChild;
-        } else { // go right
-          cNode = cNode.rightChild;
-        }
-      }
-    }
-    return cNode;
-  }
+	public Node maximum() {
+		Node cNode = null;
+		if (root == null) {
+			return null;
+		} else {
+			cNode = root;
+			while (cNode.rightChild != null) { // Keep going right till right
+												// child of node is null
+				cNode = cNode.rightChild;
+			}
+		}
+		return cNode;
+	}
 
+	public void inOrderTraversal(String order) {
+		if (order.equalsIgnoreCase("ascend")) {
+			recursiveInOrderAscendTraversal(root);
+		} else if (order.equalsIgnoreCase("dscend")) {
+			recursiveInOrderDscendTraversal(root);
+		}
+	}
 
-  public Node minimum() {
-    Node cNode = null;
-    if (root == null) {
-      return null;
-    } else {
-      cNode = root;
-      while (cNode.leftChild != null) { // keep going left till the left
-                                        // child of a node is NULL
-        cNode = cNode.leftChild;
-      }
-    }
-    return cNode;
-  }
+	private void recursiveInOrderAscendTraversal(Node cNode) {
+		if (cNode != null) {
+			recursiveInOrderAscendTraversal(cNode.leftChild);
+			System.out.println(cNode.key);
+			recursiveInOrderAscendTraversal(cNode.rightChild);
+		}
+	}
 
+	private void recursiveInOrderDscendTraversal(Node cNode) {
+		if (cNode != null) {
+			recursiveInOrderDscendTraversal(cNode.rightChild);
+			System.out.println(cNode.key);
+			recursiveInOrderDscendTraversal(cNode.leftChild);
+		}
+	}
 
-  public Node maximum() {
-    Node cNode = null;
-    if (root == null) {
-      return null;
-    } else {
-      cNode = root;
-      while (cNode.rightChild != null) { // Keep going right till right
-                                         // child of node is null
-        cNode = cNode.rightChild;
-      }
-    }
-    return cNode;
-  }
+	public void displayTree() {
 
-
-  public void inOrderTraversal(String order) {
-    if (order.equalsIgnoreCase("ascend")) {
-      recursiveInOrderAscendTraversal(root);
-    } else if (order.equalsIgnoreCase("dscend")) {
-      recursiveInOrderDscendTraversal(root);
-    }
-  }
-
-  private void recursiveInOrderAscendTraversal(Node cNode) {
-    if (cNode != null) {
-      recursiveInOrderAscendTraversal(cNode.leftChild);
-      System.out.println(cNode.key);
-      recursiveInOrderAscendTraversal(cNode.rightChild);
-    }
-  }
-
-  private void recursiveInOrderDscendTraversal(Node cNode) {
-    if (cNode != null) {
-      recursiveInOrderDscendTraversal(cNode.rightChild);
-      System.out.println(cNode.key);
-      recursiveInOrderDscendTraversal(cNode.leftChild);
-    }
-  }
-
-  public void displayTree() {
-   
-
-  }
+	}
 
 }
