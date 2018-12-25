83134186aa5abbb186ad99fd99f55f2f8900108d
RaviTella <ravi.tella@outlook.com>
2018-12-25 13:33:36 -0600

    selection sort
    Both iterative and recursive

--- src/algorithms/sorting/Factorial.java
+++ src/algorithms/sorting/Factorial.java
@@ -2,19 +2,16 @@ package algorithms.sorting;
 
 public class Factorial {
 
-	public static void main(String[] args) {
-		System.out.println(recursiveFactorial(3));
+  public static void main(String[] args) {
+    System.out.println(recursiveFactorial(3));
+  }
 
-	}
-
-	private static int recursiveFactorial(int n) {
-		// BASE CASE
-		if (n == 1) {
-			return 1;
-		}
-		//ACTION
-		return n * recursiveFactorial(n - 1);
-
-	}
-
+  private static int recursiveFactorial(int n) {
+    // BASE CASE
+    if (n == 1) {
+      return 1;
+    }
+    // ACTION
+    return n * recursiveFactorial(n - 1);
+  }
 }
--- src/algorithms/sorting/Sort.java
+++ src/algorithms/sorting/Sort.java
@@ -7,22 +7,21 @@ public class Sort {
 		this.iArray = iArray;
 	}
 
-	public void mergeSort() {
+	public void mergeSortRecursive() {
 		recursiveMerge(0, iArray.length - 1, new int[iArray.length]);
 	}
 
 	private void recursiveMerge(int sIndex, int eIndex, int[] tArray) {
-		//BASE CASE
+		// BASE CASE
 		if (sIndex == eIndex) {
 			return;
 		}
-        //DIVIDE
+		// DIVIDE
 		int mIndex = (sIndex + eIndex) / 2;
 		recursiveMerge(sIndex, mIndex, tArray);
 		recursiveMerge(mIndex + 1, eIndex, tArray);
-		//ACTION
+		// ACTION
 		merge(sIndex, mIndex, eIndex, tArray);
-
 	}
 
 	private void merge(int sIndex, int mIndex, int eIndex, int[] tArray) {
@@ -73,6 +72,72 @@ public class Sort {
 			System.out.println(e);
 
 		}
+	}
+
+	public void selectionSortIterative() {
+		// last index of the sorted sub array
+		int outer = 0;
+		while (outer < iArray.length - 1) {
+			// assume first element as the smallest
+			int smallest = outer;
+			int inner = outer + 1;
+			// find the index of the smallest value
+			while (inner < iArray.length) {
+				if (iArray[smallest] > iArray[inner]) {
+					smallest = inner;
+				}
+				inner++;
+			}
+			// swap if the assumed smallest element is not the smallest element
+			if (smallest != outer) {
+				swap(outer, smallest);
+			}
+			// Increment the sorted sub array
+			outer++;
+		}
+	}
+
+	public void selectionSortRecursive() {
+		selectionSortRecur(0, iArray.length - 1);
+	}
+
+	// outer Loop replacement
+	private void selectionSortRecur(int outer, int end) {
+		if (outer == end) {
+			return;
+		}
+		int smallest = outer;
+		int inner = outer + 1;
+		// find the index of the smallest value
+		smallest = selectionSortRecurInner(smallest, inner, end);
+		// swap if the assumed smallest element is not the smallest element
+		if (smallest != outer) {
+			swap(outer, smallest);
+		}
+		// Increment the sorted sub array
+		outer++;
+		// Recursive calls till the end of the outer loop is reached
+		selectionSortRecur(outer, end);
+	}
+
+	// Inner Loop replacement
+	private int selectionSortRecurInner(int smallest, int inner, int end) {
+		if (inner > end) {
+			return smallest;
+		}
+		if (iArray[inner] < iArray[smallest]) {
+			smallest = inner;
+		}
+		inner++;
+		return selectionSortRecurInner(smallest, inner, end);
+	}
+
+	private void swap(int x, int y) {
+		int temp = iArray[x];
+
+		iArray[x] = iArray[y];
+		iArray[y] = temp;
+
 	}
 
 }
