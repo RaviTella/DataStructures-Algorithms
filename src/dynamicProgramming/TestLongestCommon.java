7140448de0582ffe12b9b9fc1bf6d11ff40ea19c
RaviTella <ravi.tella@outlook.com>
2019-01-09 21:26:22 -0600

    name change

--- src/dynamicProgramming/TestLongestCommon.java
+++ src/dynamicProgramming/TestLongestCommon.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestLongestCommonSubstring {
+public class TestLongestCommon {
 
 	@Test
 	public void testSubStringIterativeDP() {
