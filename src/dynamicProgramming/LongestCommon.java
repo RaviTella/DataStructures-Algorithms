af6e3ddfc0d75adb6009382da88f4891958c0911
RaviTella <ravi.tella@outlook.com>
2019-01-09 21:24:47 -0600

    non-DP iterative

--- src/dynamicProgramming/LongestCommon.java
+++ src/dynamicProgramming/LongestCommon.java
@@ -41,4 +41,29 @@ public class LongestCommon {
 
 	}
 
+	public static int subStringIterative(String s1, String s2) {
+		char[] c1 = s1.toCharArray();
+		char[] c2 = s2.toCharArray();
+		int c1Len = c1.length;
+		int c2Len = c2.length;
+		int longest = 0;
+
+		for (int i = 0; i < c1Len; i++) {
+			for (int j = 0; j < c2Len; j++) {
+				if (c1[i] == c2[j]) {
+					int substring = 1;
+					for (int k = i+1, l = j+1; k < c1Len && l < c2Len; k++, l++) {
+						if (c1[k] == c2[l]) {
+							substring++;
+						} else {
+							break;
+						}
+					}
+					longest = (substring > longest) ? substring : longest;
+				}
+			}
+		}
+		return longest;
+	}
+
 }
--- src/dynamicProgramming/TestLongestCommonSubstring.java
+++ src/dynamicProgramming/TestLongestCommonSubstring.java
@@ -13,7 +13,21 @@ public class TestLongestCommonSubstring {
 		assertEquals(1, LongestCommon.subStringIterativeDP("abcplka", "axyz"));
 		assertEquals(6, LongestCommon.subStringIterativeDP("ababcplka", "axyzababkpababcp"));
 
+
 	}
+	
+	@Test
+	public void testSubStringIterative() {
+		int result = LongestCommon.subStringIterative("hello", "ello");
+		assertEquals(4, result);
+		assertEquals(1, LongestCommon.subStringIterative("abcplka", "axyz"));
+		assertEquals(6, LongestCommon.subStringIterative("ababcplka", "axyzababkpababcp"));
+		assertEquals(5, LongestCommon.subStringIterative("GeeksforGeeks", "GeeksQuiz"));
+		assertEquals(8, LongestCommon.subStringIterative("tutorialhorizon", "dynamictutorialProgramming"));
+		assertEquals(6, LongestCommon.subStringIterative("hksgtsncpdjkhwprnfkabcdefioedqwervbfdnpkpnvwfjkl", "okhdwuernaqpgfbabcdewpntryuekbndperabcdef"));
+
+	}
+	
 	
 	
 	@Test
